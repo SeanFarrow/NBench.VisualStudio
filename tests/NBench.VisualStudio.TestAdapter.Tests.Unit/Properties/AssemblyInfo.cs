e0d3f2fb8d887132f5f36b26198166c42880f134
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2016-05-05 00:57:57 +0100

    Add a failing test checking that the source collection is not null.

--- tests/NBench.VisualStudio.TestAdapter.Tests.Unit/Properties/AssemblyInfo.cs
+++ tests/NBench.VisualStudio.TestAdapter.Tests.Unit/Properties/AssemblyInfo.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("JustBehave.Tests")]
-[assembly: AssemblyCompany("Just Eat")]
-[assembly: AssemblyCopyright("Copyright 2011-2014 Just Eat, Inc. or its affiliates. All Rights Reserved.")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.0")]
 
-[assembly: CLSCompliant(false)]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Riskscoper.Server.Core.DependencyInjection.Tests.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Riskscoper.Server.Core.DependencyInjection.Tests.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d73d473f-2347-468d-9e79-766b2e35a070")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
