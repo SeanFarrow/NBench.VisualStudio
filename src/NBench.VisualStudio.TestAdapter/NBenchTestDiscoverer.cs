1e28fc4a9a1f3a745d37469ce48639c65dade4ac
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2016-05-04 23:18:27 +0100

    Add the NBench visual studio test adapter project.

---
+++ src/NBench.VisualStudio.TestAdapter/NBenchTestDiscoverer.cs
@@ -0,0 +1,15 @@
+﻿namespace NBench.VisualStudio.TestAdapter
+{
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+    using System;
+    using System.Collections.Generic;
+
+    public class NBenchTestDiscoverer : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
