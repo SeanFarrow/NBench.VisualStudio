1e10843e6275620266ba65b841d7fb6aa0c7e2df
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2016-05-04 22:17:08 +0100

    Add the NBench test adapter.

--- src/NBench.VisualStudio.TestAdapter/Properties/AssemblyInfo.cs
+++ src/NBench.VisualStudio.TestAdapter/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using NUnit.Framework;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Riskscoper.Server.Services.Elastic.NodeUtilities.Tests.Unit")]
+[assembly: AssemblyTitle("NBench.VisualStudio.TestAdapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Riskscoper.Server.Services.Elastic.NodeUtilities.Tests.Unit")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("NBench.VisualStudio.TestAdapter")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,7 +20,7 @@ using NUnit.Framework;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e82ad1c-01a5-4bf5-aa9c-3844b69270cf")]
+[assembly: Guid("07d7410f-b98f-412d-995a-230e023afd62")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,6 +32,5 @@ using NUnit.Framework;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly:Description("Unit tests for the ElasticSearch node utilities")]
