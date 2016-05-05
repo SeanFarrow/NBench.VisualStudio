c2fe209ab4ba67a6f07ec6a0af1da4c64c1b0416
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2016-05-05 01:39:44 +0100

    Make the tests pertaining to the test case discovery sink being null pass.

--- src/NBench.VisualStudio.TestAdapter/NBenchTestDiscoverer.cs
+++ src/NBench.VisualStudio.TestAdapter/NBenchTestDiscoverer.cs
@@ -26,6 +26,13 @@ using System.Linq;
             {
                 throw new ArgumentNullException("logger", "The message logger you have passed in is null. The message logger must not be null.");
             }
+            else if (discoverySink == null)
+            {
+                throw new ArgumentNullException(
+                    "discoverySink",
+                    "The test case discovery sink you have passed in is null. The test case discovery sink must not be null.");
+            }
+
             throw new NotImplementedException();
         }
     }
